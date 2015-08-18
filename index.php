0b591f6825fd7fbd8a2f0216e643fa28f5e2c2fe
Diamond00744 <diamond00744@gmail.com>
2015-08-18 18:54:25 +0300

    Main html+css layout: header (logo + navbar example), body (content sample + sidebar sample), footer sample

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Achi Test Service</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<div id="header_wrapper">
+				<div id="header_logo"></div>
+				<nav id="header_menu">
+					<ul>
+						<li><a href="feed.php" class="here">Лента</a></li>
+						<li><a href="friends.php">Друзья</a></li>
+						<li><a href="profile.php">Профиль</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div id="content">
+			<div id="content_wrapper">
+				<div id="content_sidebar">
+					<div class="block">Новое достижение</div>
+					<div class="block">Diamond00744<br>> профиль<br>> настройки<br>> выйти</div>
+					<div class="block">Вы наполучали: сегодня одно, вчера 3, 32 декабря: 5 штук</div>
+				</div>
+				<div id="content_main">
+					<h1>Лента новостей</h1>
+					<h2>Сегодня</h2>
+					<div class="block">Хмырь ID#555 получил два достижения</div>
+					<div class="block">Хмырь ID#575 получил достижение "Похититель"</div>
+					<div class="block">Хмырь ID#1 получил достижение "ДуровЛох"</div>
+					<h2>Вчера</h2>
+					<div class="block">ID#440 добавил в друзья ID#441</div>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<div id="footer_wrapper">
+				Подвал сайта, копирайты (C) 2015 от Даймонда00744
+			</div>
+		</div>
+	</div>
+</body>
+</html>
