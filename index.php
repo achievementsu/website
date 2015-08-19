e0b5e16e0b98ede9289d1b1252e16b19222ffe90
Diamond00744 <diamond00744@gmail.com>
2015-08-19 10:50:28 +0300

    Sidebar login form

--- index.php
+++ index.php
@@ -22,6 +22,17 @@
 			<div id="content_wrapper">
 				<div id="content_sidebar">
 					<div class="sideblock sideblock-new"><a href="new.php" class="new_button">Новое достижение</a></div>
+					<div class="sideblock sideblock-login">
+						<form id="login-form" name="logon" method="POST">
+							<label for="login-username">Имя пользователя</label><br>
+							<input id="login-username" type="text" tabindex="1" maxlength="50" name="username"></input><br><br>
+							<label for="login-password">Пароль</label><br>
+							<input id="login-password" type="password" tabindex="2" maxlength="50" autocomplete="off" name="password"></input><br><br>
+							<input id="login-button" type="submit" tabindex="3" value="Вход"></input>
+						</form>
+						Восстановление доступа<br>
+						Регистрация
+					</div>
 					<div class="sideblock sideblock-profile">
 						<div class="title">Diamond00744</div>
 						<div class="avatar"><img src="storage/avatars/diamond00744.jpg"></div>
