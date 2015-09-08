655aefae652ddd35fbefdfb0e244e588238465fa
Diamond00744 <diamond00744@gmail.com>
2015-09-08 21:34:16 +0300

    Profile page example

--- profile.php
+++ profile.php
@@ -8,15 +8,41 @@
 <h1>Профиль Diamond00744</h1>
 <h2>Информация</h2>
 <div class="section">
-
+	<div class="avatar">
+		<div class="level">99</div>
+		<img src="storage/avatars/diamond00744.jpg">
+	</div>
+	<div class="profile-info">
+		<div>Геродот Кузьмич Подкова, 25 лет</div>
+		<div>Я не указал никакой информации о себе</div>
+	</div>
 </div>
 <h2>Общая статистика</h2>
-<div class="section">
-
+<div class="section profile-stats">
+	<div>Достижений: 17</div>
+	<div>Уровень: 99</div>
 </div>
 <h2>Последние достижения</h2>
-<div class="section">
-
+<div class="section achievements-list">
+	<div class="achievement-full">
+		<div class="icon">
+			<img src="storage/icons/randomcode.jpg">
+		</div>
+		<div class="info">
+			<div class="name">Яки Иезус</div>
+			<div class="description">С днём рождения!</div>
+			<div class="meta">
+				<div class="level">Уровень: 1</div>
+				<div class="sender">Прислал: The God</div>
+				<div class="time">25 декабря 0 года (получено 7 января 1 года)</div>
+			</div>
+		</div>
+	</div>
+	<div class="center-buttons">
+		<div class="center-button">
+			<a href="achievements.php?id=diamond00744">Все достижения</a>
+		</div>
+	</div>
 </div>
 
 <?php require_once 'include/static/footer.php'; ?>
