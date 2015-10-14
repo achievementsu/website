bb87c2d197ca2236ea0e5c6804ff5c4e9780adb3
Diamond00744 <diamond00744@gmail.com>
2015-10-14 23:15:56 +0300

    Achievement adding page extended

--- add.php
+++ add.php
@@ -7,6 +7,22 @@
 
 <h1>Добавить новое достижение</h1>
 <form id="add" class="add">
+	<h2>Адресант достижения</h2>
+	<div class="section">
+			<div class="setting">
+				<label class="setting-label" for="setting-to">Достижение для</label>
+				<div class="setting-control">
+					<input id="setting-to" name="achievement[to]" value="">
+				</div>
+			</div>
+					<div class="setting">
+						<label class="setting-label" for="setting-time">Достижение получил</label>
+						<div class="setting-control">
+							<input id="setting-time" name="achievement[time]" value="">
+						</div>
+					</div>
+	</div>
+	<h2>Подробности достижения</h2>
 	<div class="section">
 		<div class="setting">
 			<label class="setting-label" for="setting-name">Название достижения</label>
@@ -20,8 +36,20 @@
 				<input id="setting-description" name="achievement[description]" value="">
 			</div>
 		</div>
+		<div class="setting">
+			<label class="setting-label" for="setting-level">Выберите уровень достижения</label>
+			<div class="setting-control">
+				<input id="setting-level" name="achievement[level]" value="">
+			</div>
+		</div>
+		<div class="setting">
+			<label class="setting-label" for="setting-color">Выберите цвет достижения</label>
+			<div class="setting-control">
+				<input id="setting-color" name="achievement[color]" value="">
+			</div>
+		</div>
 		<div class="actions">
-			<input type="submit" value="Сохранить">
+			<input type="submit" value="Отправить">
 		</div>
 </form>
 
