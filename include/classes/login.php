e1045babe2ac3d393ded5ab647e1dafb27da86ea
Diamond00744 <diamond00744@gmail.com>
2015-11-09 08:59:08 +0300

    Login class that stores current logged in user
    Signed-off-by: Diamond00744 <diamond00744@gmail.com>

--- include/classes/login.php
+++ include/classes/login.php
@@ -3,8 +3,24 @@
 /* Необходим класс пользователь */
 require_once 'user.php';
 
-class Login {
+class Login
+{
+	public $user;
 
+	public function __construct() {
+		$user = NULL;
+
+		global $db;
+		if (($_COOKIE['id']) && ($_COOKIE['password'])) {
+			$query = "SELECT password FROM achi_users WHERE id=" . $_COOKIE['id'];
+			$data = $db->query($query)->fetch_assoc();
+
+			if (($data) && ($_COOKIE['password'] == $data['password']))
+				$user = new User($_COOKIE['id']);
+		}
+	}
 }
+
+$login = new Login();
 
 ?>
