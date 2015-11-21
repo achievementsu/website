91c6861eb1caf5da4d656c920d5864a7d2bdb0c4
Diamond00744 <diamond00744@gmail.com>
2015-11-21 19:39:32 +0300

    URL rewrite sketch (http://stackoverflow.com/questions/16388959/url-rewriting-with-php)

---
+++ fallback.php
@@ -0,0 +1,22 @@
+<?php
+
+$path = ltrim($_SERVER['REQUEST_URI'], '/'); //Обрезка начального слеша(-ей)
+$elements = explode('/', $path); //Делим путь по слешам
+
+if(count($elements) == 0)
+  header('Location: index.php');
+else switch(array_shift($elements)) //Выдираем первый элемент из массива и переключаем его
+{
+  case 'profile':
+    if ($elements[0]) {
+      header('Location: /profile.php?id=' . $elements[0]);
+    } else {
+      header('Location: /profile.php');
+    }
+    break;
+  default:
+    //header('Location: index.php?error404');
+    //header('HTTP/1.1 404 Not Found');
+}
+
+?>
