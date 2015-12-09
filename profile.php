4899610917d3488b8d3244d2d6fda8b33f91efc2
Diamond00744 <diamond00744@gmail.com>
2015-12-09 20:29:30 +0300

    Show achievements in profile page

--- profile.php
+++ profile.php
@@ -7,6 +7,32 @@ if (!isset($login->user)) {
 	header('Location: index.php');
 }
 
+function showAchievementsList($id) {
+	global $db, $login;
+
+	$query = 'SELECT * FROM `achi_achievements` WHERE `to`=' . $id . ' ORDER BY `time_sent` DESC LIMIT 0 , 5';
+	$result = $db->query($query);
+
+	while ($data = $result->fetch_assoc()) {
+	?>
+	<div class="achievement-full" style="border-color: <?php echo $data['color'] ?>;">
+		<div class="icon" style="border-color: <?php echo $data['color'] ?>;">
+			<img src="storage/icons/<?php echo $data['image'] ?>">
+		</div>
+		<div class="info">
+			<div class="name"><?php echo $data['name'] ?></div>
+			<div class="description"><?php echo $data['description'] ?></div>
+			<div class="meta">
+				<div class="level">Уровень: <?php echo $data['level'] ?></div>
+				<div class="sender">Прислал: The God</div>
+				<div class="time"><?php echo $data['time_set'] ?> (получено <?php echo $data['time_sent'] ?>)</div>
+			</div>
+		</div>
+	</div>
+	<?php
+	}
+}
+
 if ($_GET['id'] && $_GET['id'] != $login->user->id) {
 	$user = new User($_GET['id']);
 	$title = 'Профиль ' . $user->username;
@@ -54,25 +80,7 @@ if (!isset($user->id)) {
 </div>
 <h2>Последние достижения</h2>
 <div class="section achievements-list">
-	<div class="achievement-full">
-		<div class="icon">
-			<img src="storage/icons/randomcode.jpg">
-		</div>
-		<div class="info">
-			<div class="name">Яки Иезус</div>
-			<div class="description">С днём рождения!</div>
-			<div class="meta">
-				<div class="level">Уровень: 1</div>
-				<div class="sender">Прислал: The God</div>
-				<div class="time">25 декабря 0 года (получено 7 января 1 года)</div>
-			</div>
-		</div>
-	</div>
-	<div class="achievement-full">
-		<div class="info">
-			<div class="name">И таких всего на этой странице пять</div>
-		</div>
-	</div>
+	<?php showAchievementsList($user->id); ?>
 	<div class="center-buttons">
 		<a href="achievements.php?id=<?php echo $user->id; ?>" class="center-button">Все достижения</a>
 	</div>
