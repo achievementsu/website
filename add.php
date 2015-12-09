0980e111f8de1b397c68f718a7bcfde71f236c39
Diamond00744 <diamond00744@gmail.com>
2015-12-09 18:42:54 +0300

    Add achievement works!
    Signed-off-by: Diamond00744 <diamond00744@gmail.com>

--- add.php
+++ add.php
@@ -12,7 +12,7 @@ $current_page = 'add';
 $showSidebar = true;
 
 function sendAchievement() {
-	global $login, $listMessages;
+	global $db, $login, $listMessages;
 
 	// Валидность получателя
 	if (!($_POST['to'] == $login->user->id || User::isFriends($login->user->id, $_POST['to']))) {
@@ -20,7 +20,7 @@ function sendAchievement() {
 			'type' => 'error',
 			'description' => 'Получатель достижения должен являться Вашим другом или Вами.'
 		);
-		return;
+		return false;
 	}
 
 	// Разбор даты
@@ -32,7 +32,7 @@ function sendAchievement() {
 			'type' => 'error',
 			'description' => 'Дата указана в неприемлемом формате. Рекомендуемый формат: ГГГГ-ММ-ДД ЧЧ:ММ:СС'
 		);
-		return;
+		return false;
 	}
 	if ($_POST['time'] && strtotime($_POST['time']) && (strtotime($_POST['time'])-($login->user->timezone * 3600) > time())) {
 		$listMessages[] = array(
@@ -40,8 +40,7 @@ function sendAchievement() {
 			'description' => 'Вы не можете отправлять достижения будущего :)<br>Время было возвращено на круги своя.'
 		);
 		$_POST['time'] = date('Y-m-d H:i:s', time()+($login->user->timezone * 3600));
-		//$_POST['time'] = date('Y.m.d H:i:s', time()+($login->user->timezone * 3600));
-		return;
+		return false;
 	}
 
 	if (!$_POST['name']) {
@@ -49,14 +48,14 @@ function sendAchievement() {
 			'type' => 'error',
 			'description' => 'Необходимо ввести название достижения.'
 		);
-		return;
+		return false;
 	}
 	if (!$_POST['description']) {
 		$listMessages[] = array(
 			'type' => 'error',
 			'description' => 'Необходимо описать достижение.'
 		);
-		return;
+		return false;
 	}
 
 	if (
@@ -65,6 +64,113 @@ function sendAchievement() {
 	) {
 		$_POST['color'] = '000000';
 	}
+
+	if (
+	!isset($_FILES['icon']['error']) ||
+	is_array($_FILES['icon']['error'])
+	) {
+		$listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: неподобающие параметры...'
+		);
+		return false;
+	}
+	switch ($_FILES['icon']['error']) {
+		case UPLOAD_ERR_OK: break;
+		case UPLOAD_ERR_NO_FILE: $listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: файл не был отправлен...'
+		);
+		return false;
+		case UPLOAD_ERR_INI_SIZE:
+		case UPLOAD_ERR_FORM_SIZE: $listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: превышен максимальный размер...'
+		);
+		return false;
+		default: $listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: неизвестная ошибка...'
+		);
+		return false;
+	}
+
+	if ($_FILES['icon']['size'] > 30000) {
+		$listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: превышен максимальный размер...'
+		);
+		return false;
+	}
+
+	$size = getimagesize($_FILES['icon']['tmp_name']);
+	$mime = $size['mime'];
+	switch ($mime) {
+		case 'image/jpeg': $ext = 'jpg'; break;
+		case 'image/png': $ext = 'png'; break;
+		case 'image/gif': $ext = 'gif'; break;
+		default:
+		$listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: файл не является изображением.'
+		);
+		return false;
+	}
+	if ($size[0] != 64 || $size[1] != 64) {
+		$listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: размер изображения отличается от 64*64.'
+		);
+		return false;
+	}
+
+	/*$finfo = new finfo(FILEINFO_MIME_TYPE);
+	if (false === $ext = array_search(
+		$finfo->file($_FILES['icon']['tmp_name']),
+		array(
+			'jpg' => 'image/jpeg',
+			'png' => 'image/png',
+			'gif' => 'image/gif',
+		),
+		true
+	)) {
+		$listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки файла: формат файла неприемлем...'
+		);
+		return false;
+	}*/
+
+	$fileName = sprintf('%s.%s',
+		sha1_file($_FILES['icon']['tmp_name']),
+		$ext
+	);
+
+	if (!move_uploaded_file(
+		$_FILES['icon']['tmp_name'], 'storage/icons/' . $fileName
+	)) {
+		$listMessages[] = array(
+			'type' => 'error',
+			'description' => 'Ошибка загрузки иконки: не удалось переместить загруженный файл...'
+		);
+		return false;
+	}
+
+	$timeset = date('Y-m-d H:i:s', strtotime($_POST['time'])-($login->user->timezone * 3600));
+	$timesent = date('Y-m-d H:i:s', time());
+
+	$query = 'INSERT INTO achi_achievements (`from`, `to`, `status`, `name`, `description`, `color`, `time_sent`, `time_set`, `level`, `image`) ';
+	$query .= 'VALUES("' . $login->user->id . '", "' . $_POST['to'] . '", "1", "' . $_POST['name'] . '", "' . $_POST['description'] . '", ';
+	$query .= '"' . $_POST['color'] . '", "' . $timesent . '", "' . $timeset . '", "' . $_POST['level'] . '", "' . $fileName . '")';
+	if ($db->query($query)) {
+		$listMessages[] = array(
+			'type' => 'success',
+			'description' => 'Достижение успешно отправлено!'
+		);
+		return true;
+	}
+
+	return false;
 }
 
 if (!$_POST['to']) {
@@ -72,10 +178,10 @@ if (!$_POST['to']) {
 		$_POST['to'] = $login->user->id;
 	}
 }
-if ($_POST['level'] < 1) {
-	$_POST['level'] = 1;
-} elseif ($_POST['level'] > 10) {
+if ($_POST['level'] > 10) {
 	$_POST['level'] = 10;
+} elseif ($_POST['level'] < 1 || (!preg_match("|^[0-9]+$|i", $_POST['level']))) {
+	$_POST['level'] = 1;
 }
 
 if ($_POST['send']) {
