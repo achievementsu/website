40b8bac81d21143ed583e78c6433edf3e7244051
Diamond00744 <diamond00744@gmail.com>
2015-12-13 22:31:27 +0300

    Missing pages now not missing! About, blog, contact, legal notice, terms + full logo and image for first blog post

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'include/functions.php';
+
+$title = 'Связаться с администрацией';
+$current_page = 'contact';
+$showSidebar = false;
+
+Markup::pageStart();
+
+?>
+
+<p>
+	Связаться с администрацией можно по этому адресу: <a href="mailto:diamond00744@gmail.com">diamond00744@gmail.com</a>
+</p>
+<p>
+	Также можете решить возникшие вопросы в <a href="http://vk.com/achievementsu">группе В Контакте</a>.
+</p>
+
+<?php
+
+Markup::pageEnd();
+
+?>
